53f5a7ee9fa958f1fe6d4e397fb980db5be53efd
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-02 18:09:15 +0300

    making modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 import sys
-for i in sys.argv:
-    print(i)
+print("{} arguments:".format(len(sys.argv) - 1))
+for i in range(1, len(sys.argv)):
+    print("{}: {}".format(i, sys.argv[i]))
