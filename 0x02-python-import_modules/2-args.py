ba3c7427faf88d584c237bb5a43b2d4dd18fad24
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-02 18:18:40 +0300

    making modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 import sys
-print("{} arguments:".format(len(sys.argv) - 1))
-for i in range(1, len(sys.argv)):
-    print("{}: {}".format(i, sys.argv[i]))
+if (len(sys.argv) == 1):
+    print("0 arguments.")
+elif (len(sys.argv) == 2):
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
