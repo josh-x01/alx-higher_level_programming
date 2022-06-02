27275bf472233fd7b25ff5165d84706fad7946ee
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-02 19:51:11 +0300

    making modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
+    import sys
     hiddenList = dir(hidden_4)
+    hiddenList.sort()
     
-    for i in range(len(hiddenList) -1):
+    for i in range(len(hiddenList) - 1):
         if (hiddenList[i][0] != '_'):
             print(hiddenList[i])
