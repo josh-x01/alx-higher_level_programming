cec2ee5a13f4cd04833962c23bec66d1b6691048
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-04 11:35:33 +0300

    data structure

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    len_sen = len(sentence)
     if (sentence == ""):
-        first = None
-    else:
-        first = sentence[0]
-        return (len_sen, first)
+        return (0, None)
+    return (len(senetence), sentence[0])
