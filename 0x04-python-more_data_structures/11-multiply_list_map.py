6b2c05ad1efdab2add73ba7dbea48fb841daedcf
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-09 02:09:34 +0300

    More on Datastructure

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    return sum([x*y for (x, y) in my_list]) / sum([y for (x, y) in my_list])
