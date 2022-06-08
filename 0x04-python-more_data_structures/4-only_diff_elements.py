7fa93c897d2687562d422f3a388a15f5e9b230ec
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-08 21:03:50 +0300

    More on Datastructure

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,7 +4,8 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         all_set.append(i)
     for j in set_2:
-        if j in set_2:
+        if j in all_set:
             all_set.remove(j)
         else:
-            all_append(j)
+            all_set.append(j)
+    return all_set
