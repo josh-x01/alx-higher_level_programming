46dd0a7ed3355c40b1502caecf9ee166782a15e9
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-08 20:33:40 +0300

    More on data structure

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            new_matrix[i][j] = matrix[i][j] ** 2
+    return new_matrix
