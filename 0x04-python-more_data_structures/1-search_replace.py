cbccfb5c22b2df144f5874c9f7adf9f3cde94323
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-08 20:34:02 +0300

    More on Datastructure

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    copy_list = my_list[:]
+    for i in copy_list:
+        if i == search:
+            i = replace
+            return copy_list
