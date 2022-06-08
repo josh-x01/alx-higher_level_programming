02dd4bf0eb46d396a8bc464561807ca5fecd1651
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-09 01:40:49 +0300

    More on Datastructure

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-update_dictionary = __import__('7-update_dictionary').update_dictionary
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+multiply_by_2 = __import__('9-multiply_by_2').multiply_by_2
+print_sorted_dictionary = \
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
-print_sorted_dictionary(new_dict)
-print("--")
+a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+new_dict = multiply_by_2(a_dictionary)
 print_sorted_dictionary(a_dictionary)
-
 print("--")
-print("--")
-
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
 print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
