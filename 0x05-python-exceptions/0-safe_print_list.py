dc6f7decf21b2ff178a1548a395f5fb210bf1649
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-20 15:58:43 +0300

    error handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             num += 1
         except IndexError:
-            pass
+            break
     print("")
     return num
