453ad5e368555f48941d82ffd63e4c9ffb894425
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-20 16:38:37 +0300

    error handling

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/bin/bash/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            num += 1
+        except ValueError:
+            pass
+        except IndexError:
+            raise
+        except TypeError:
+            pass
+    print("")
+    return num
