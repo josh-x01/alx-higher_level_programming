7a477656d1f75ddefef343b3e0e3e03abefecade
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-20 19:21:40 +0300

    error handling

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
+    except TypeError as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
