0638c618446a00dcba0fa6890e3ffc5bc693f938
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-21 19:21:18 +0300

    error handling

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,49 +4,80 @@
 
 
 class Square:
-
+    """ Empty class with size private attribute
+    """
     def __init__(self, size=0, position=(0, 0)):
+        """
+                Instantiation with size
+        Args:
+            size: size of the square
+            position: postion of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
-        self.__position = position
+        self.position = position
+
+    def area(self):
+        """
+        Returns the area of the square
+        """
+        return (self.__size * self.__size)
 
     @property
     def size(self):
+        """
+        size getter. Handle size errors
+        """
         return self.__size
-
-    @property
-    def position(self):
-        return self.__position
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        """
+        size setter. Set the size square
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
+
+    def my_print(self):
+        """
+        Print a square with the character # at position given
+        """
+        if self.__size == 0:
+            print()
+            return
+        for j in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
+
+    @property
+    def position(self):
+        """
+        position setter. Set the position of square
+        """
+        return self.__position
 
     @position.setter
     def position(self, value):
-        Error = TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2:
-            raise Error
-        elif type(value[0]) == int and type(value[1]) == int:
-            if value[0] < 0 or value[1] < 0:
-                raise Error
-            else:
-                self.__position = value
+        """
+        Handle position with errors
+        """
+        if type(value) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif isinstance(value[0], int) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif isinstance(value[1], int) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            raise Error
-
-    def area(self):
-        return self.__size ** 2
-
-    def my_print(self):
-        if self.__size == 0:
-            print("")
-        else:
-            print("\n" * self.__position[1], end="")
-            for i in range(self.__size):
-                print(" " * self.__position[0], end="")
-                print("#" * self.__size)
+            self.__position = value
