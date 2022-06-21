b85a138c0eadbe351787541bc69a38426b52e3d9
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-21 19:19:56 +0300

    error handling

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,31 +4,52 @@
 
 
 class Square:
-
+    """ Empty class with size private attribute
+    """
     def __init__(self, size=0):
+        """
+                Instantiation with size
+        Args:
+            size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        return self.__size ** 2
-
-    def my_print(self):
-        if self.__size == 0:
-            print("")
-        else:
-            for num in range(self.__size):
-                for num in range(self.__size):
-                    print("#", end="")
-                print("")
+        """
+        Returns the area of the square
+        """
+        return (self.__size * self.__size)
 
     @property
     def size(self):
+        """
+        size getter. Handle size errors
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        """
+        size setter. Set the size square
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """
+        Print a square with the character #
+        """
+        if self.__size == 0:
+            print()
         else:
-            self.__size = value
+            for row in range(self.__size):
+                for column in range(self.__size):
+                    print("#", end="")
+                print()
