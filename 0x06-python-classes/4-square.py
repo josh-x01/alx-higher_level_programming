a5669d59b744df7d74b884ae2c411c48365e4582
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-21 19:18:31 +0300

    error handling

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,22 +4,40 @@
 
 
 class Square:
-
+    """ Empty class with size private attribute
+    """
     def __init__(self, size=0):
+        """
+                Instantiation with size
+        Args:
+            size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        return self.__size ** 2
+        """
+        Returns the area of the square
+        """
+        return (self.__size * self.__size)
 
     @property
     def size(self):
+        """
+        size getter. Handle size errors
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        """
+        size setter. Set the size square
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = 
