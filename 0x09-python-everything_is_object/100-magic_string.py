7f50f2dd8df53811e85f7482243e0399cbfeeb4c
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-06-28 22:39:47 +0300

    oop

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(STR=[]):
+    STR.append("Holberton")
+    return ",".join(STR)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+class LockedClass:
+    __slots__ = "fisrt_name"
