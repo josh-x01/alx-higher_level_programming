3df69ef40cd1705de15e33d76b7bed001592f833
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-04 23:56:54 +0300

    doc_error

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,11 +4,12 @@ checking an object is instance of a class or not."""
 
 
 def is_same_class(obj, a_class):
-    """Return: True if obj os instamce of a_class, else False.
-    
+    """A function check if object is type of class.
+
     Args:
         - obj: the object to check
         - a_class: the class where the object belong
+    Returns: True if obj os instamce of a_class, else False.
     """
     
     return True if type(obj) is a_class else False
