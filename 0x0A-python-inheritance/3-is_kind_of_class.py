517374f511ba35b78013b4401e081bb7ff70a20c
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-05 00:10:05 +0300

    isinstance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module 3-is_kind_of_class
+find object instance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check an object is instance or inherited class instance of a class.
+
+    Agrs:
+        - obj: object to check
+        - a_class: the main class to check
+
+    Return: True is the object is instance or inherited from the class.
+    """
+
+   return isinstance(obj, a_class)
