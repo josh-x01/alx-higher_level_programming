53218389670ffbec96b0ec44a8ddf75e6cd32ab8
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-05 00:41:56 +0300

    errors

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module 6-base_geomery.
+the set of error why? just because.
+"""
+
+
+class BaseGeometry:
+    """The geometry class"""
+
+    def area(self):
+        """a function the raise exception in
+        the middle of nowhere.
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """check if the value is valid integer or not
+
+        Args:
+            - name: the name
+            - value: the value
+        """
+
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
