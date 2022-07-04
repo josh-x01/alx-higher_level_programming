5e010d83d0117579646816368b38e8d9e5bb4191
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-05 01:08:07 +0300

    are_implemented

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module 9-rectangle
+width and height plus inheritance
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """rectangle class that inherits from somewhere"""
+
+    def __init__(self, width, height):
+        """instatiate the class with para width and height
+
+        Args:
+            - width: width of the rectangle
+            - height: the height of the rectangle
+        """
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__heigth = height
+    
+    def area(self):
+        """Return the area of the rectangle"""
+
+        return self.__height * self.__width
+
+    def __str__(self):
+        """Returns a formatted string"""
+
+        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
