1e351b5651add32235bb32af9dddd398ee3a10c6
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-04 23:21:30 +0300

    confused

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-Module 0-lookup
-Finding all attrubutes and methods of an object and store it in the list
+"""Module 0-lookup.
+Finding all attrubutes and methods of an object and store it in the list.
 """
 
 
 def lookup(obj):
-    """
-    Return the list of attributes and method without the their values
+    """Return the list of attributes and method without the their values
+
     Args:
         - obj: the object where the attributes and method found
     """
-    
+
     return getattr(obj)
