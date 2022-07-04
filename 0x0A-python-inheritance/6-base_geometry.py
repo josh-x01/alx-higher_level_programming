d6aa156b1f8ab3886fd6ff1f92cc42b997e1c40e
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-05 00:32:23 +0300

    raise

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module 6-base_geomery.
+get the area.
+"""
+
+
+class BaseGeometry:
+    """The geometry class"""
+
+    def area(self):
+        """a function the raise exception in
+        the middle of nowhere.
+        """
+
+        raise Exception("area() is not implemented")
