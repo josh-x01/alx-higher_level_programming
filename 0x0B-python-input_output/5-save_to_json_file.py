80a90601cbbf4fd65fb20a9e935c07cf650b0153
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-05 23:40:41 +0300

    saving

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module is not a big deal"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """who cares about a function"""
+    with open(filename, 'w') as f:
+        f.write(my_object)
