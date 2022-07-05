7584845d7e2f1f057b21dc60c4b8ea21a6d915ce
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-05 23:29:03 +0300

    json

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this module changes the string to object"""
+import json
+
+
+def from_json_string(my_str):
+    """function that changes json string to json object"""
+    try:
+        return json.loads(my_str)
+    except ValueError:
+        pass
