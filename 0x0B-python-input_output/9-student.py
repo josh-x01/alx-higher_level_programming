33a05e28c3c941969ae733ed861d74a699a0cdb4
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-06 00:44:49 +0300

    class_fixed

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,10 +3,19 @@
 
 
 class Student:
+    """define student
+    attr:
+        - firstname
+        - lastname
+        - age
+    """
     def __init__(self, firstname, lastname, age):
+        """Initializes the class with the given arguments"""
+
         self.firstname = firstname
         self.lastname = lastname
         self.age = age
 
     def to_json(self):
+        """Return the description"""
         return self.__dict__
