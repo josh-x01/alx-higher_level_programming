c40f43a155fff218808092a09db318902f366597
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-06 00:18:50 +0300

    json_size

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,14 +4,14 @@ add args to list and save it to json
 """
 import json
 import sys
-import os
+import os.path
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 _list = []
 filename = "add_item.json"
-if os.path.exists(filename):
-    _list = load_from_json_file
+if os.path.exists(filename) and os.path.getsize(filename) > 0:
+    _list = load_from_json_file(filename)
 
 if len(sys.argv) > 1:
     for i in sys.argv[1:]:
