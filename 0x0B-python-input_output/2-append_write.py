daa28bc5f98bffec5fab60b839349b26e556dd7e
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-05 21:57:44 +0300

    appending

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module 2-append_write..
+append text at the end of a file
+"""
+
+
+def append_write(filename="", text=""):
+    """append_write functionn takes filename and
+    text then add the text to filename at the end
+
+    Args:
+        - filename: the file name
+        - text: the text to be add
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        size_char = f.append(text)
+
+    f.closed
+    return size_char
