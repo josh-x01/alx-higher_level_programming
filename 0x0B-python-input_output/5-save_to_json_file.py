51a0971ceada2dffd9265635c090e478d6d27d57
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-05 23:43:25 +0300

    saving...

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """who cares about a function"""
     with open(filename, 'w') as f:
-        f.write(my_object)
+        json.dump(my_obj, f)
