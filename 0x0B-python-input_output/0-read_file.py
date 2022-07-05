328454e98560c66e318520f327e343be2a186bec
Eyasu Yidnekachew <eyasuyid@gmail.com>
2022-07-05 21:42:18 +0300

    read_from_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module 0-read_file.
+This module read text from file
+"""
+
+
+def read_file(filename=""):
+    """read_file functionn takes filename and
+    read text
+
+    Args:
+        - filename: the file name
+    """
+    
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
