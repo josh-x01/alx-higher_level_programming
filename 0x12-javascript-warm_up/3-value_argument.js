87ad384c3f58921191400c48c14e933e1939b073
Lidiya Gezahegn <lindagez>
2022-09-06 08:18:21 +0300

    done

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+
 if (process.argv.length === 2) {
   console.log('No argument');
 }
-if (process.argv.length === 3) {
+else if (process.argv.length === 3) {
   console.log(process.argv[2]);
 }
