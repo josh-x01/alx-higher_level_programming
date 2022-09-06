56783b102431333f67d44b4cd22b79d3d2b8335c
Lidiya Gezahegn <lindagez>
2022-09-06 07:20:55 +0300

    argv_s

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 
 if (process.argv.length === 2) {
   console.log('No argument');
-} else {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
