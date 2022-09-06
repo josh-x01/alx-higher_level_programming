9736430d086a835d12896e8e60f035d8e9a1374e
Lidiya Gezahegn <lindagez>
2022-09-06 08:16:19 +0300

    undefined_fixed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-console.log(process.argv[2]);
+if (process.argv.length === 2) {
+  console.log('No argument');
+}
+if (process.argv.length === 3) {
+  console.log(process.argv[2]);
+}
