75d5a1c9eae6bb15e8b99e4c57d89511b7f920c1
eyasuyid <eyasuyid@gmail.com>
2022-10-04 23:11:19 +0300

    find peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+'''Module 6-peak
+Contains function find_peak
+'''
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
-
-    if list_of_integers is None or list_of_integers == []:
+    '''Finds the biggest number in list_of_integers
+    and returns it. If the list is empty, None is returned.
+    '''
+    if len(list_of_integers) == 0:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+    return sorted(list_of_integers)[-1]
