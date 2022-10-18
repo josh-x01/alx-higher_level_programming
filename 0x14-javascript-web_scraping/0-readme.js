348461a60d1483f2a29b8012546e4d014023cd6d
eyasuyid <eyasuyid@gmail.com>
2022-10-18 22:39:26 +0300

    req fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-require('fs').readFile(process.argv[2], 'utf-8', (e, r) => {
-        if (e) console.log(e)
-        else console.log(r)
+const fs = require('fs')
+fs.readFile(process.argv[2], 'utf-8', (e, r) => {
+        if (e) console.log(e);
+        else console.log(r);
 
 });
