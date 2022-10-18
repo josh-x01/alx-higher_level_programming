43ccbe9e6805eaaf190693f9e282e7b939971163
eyasuyid <eyasuyid@gmail.com>
2022-10-18 22:36:59 +0300

    req fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
 require('fs').readFile(process.argv[2], 'utf-8', (e, r) => {
-        if (e)
-                console.log(e)
-        else
-                console.log(r)
+        if (e) console.log(e)
+        else console.log(r)
 
 });
