39028274b9c3dea6f82dc2ddd0859e26e9d08a0c
eyasuyid <eyasuyid@gmail.com>
2022-10-19 13:15:31 +0300

    loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 require('request')(process.argv[2], (e, r, b) => {
-require('fs').writeFile(process.argv[3], b, e => {if (e) console.log(e)})
-})
+  require('fs').writeFile(process.argv[3], b, e => { if (e) console.log(e); });
+});
