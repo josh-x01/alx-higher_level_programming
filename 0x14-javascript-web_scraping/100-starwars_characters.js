5da6e7d7b596c6bd7f413b5e02d18fb895ae0982
eyasuyid <eyasuyid@gmail.com>
2022-10-19 17:15:51 +0300

    power of js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  JSON.parse(body).characters.forEach(function (url, callback) {
-    request(url, function (error, response, body) {
-      if (error) {
-        console.error(error);
-      }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (e, r, b) => {
+  if (e) console.log(e);
+  JSON.parse(b).characters.forEach(url => {
+    request(url, (err, response, body) => {
+      if (err) console.log(err);
       console.log(JSON.parse(body).name);
     });
   });
