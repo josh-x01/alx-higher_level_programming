328364c54d0154b6c3701d7bfc738cc738b0fb54
Joshua <87242555+eyasuyid@users.noreply.github.com>
2022-10-19 12:54:25 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', e => {
+require('fs').writeFile(process.argv[2], process.argv[3], 'utf-8', e => {
   if (e) console.log(e);
 });
