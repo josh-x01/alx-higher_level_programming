e58726095faf2a9ba4178fd0390c7e4cab6302d5
eyasuyid <eyasuyid@gmail.com>
2022-10-19 14:32:34 +0300

    love js so badly

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 
 require('request')(process.argv[2], (e, r, b) => {
-  const CompletedTasks = {};
-  for (let i = 1; i <= 10; i++) {
-    CompletedTasks[i] = JSON.parse(b).filter(task => task.completed === true)
-      .filter(task => task.userId === i).length;
+  if (e) console.log(e);
+  else {
+    const CompletedTasks = {};
+    for (let i = 1; i <= 10; i++) {
+      CompletedTasks[i] = JSON.parse(b).filter(task => task.completed === true)
+        .filter(task => task.userId === i).length;
+    }
+    console.log(CompletedTasks);
   }
-  console.log(CompletedTasks);
 });
